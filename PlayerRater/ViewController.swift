1817ef48d5320f7e3341bca45731cc7d1c5a2e78
Jorge Raad <jar121@duke.edu>
2019-06-12 23:20:26 -0400

    Initial Commit

---
+++ PlayerRater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerRater
+//
+//  Created by Jorge Alejandro Raad on 6/12/19.
+//  Copyright © 2019 Jorge Alejandro Raad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
